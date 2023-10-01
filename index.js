4a1ea4b7e6b57d4022c8c9e62d2ba7b8f279f61c
Milan Pavic <milanpavice@gmail.com>
2023-10-01 15:02:31 +0200

    Add type module support

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Function to calculate the power of 2
-function powerOfTwo(exponent) {
+export function powerOfTwo(exponent) {
     if (exponent < 0) {
       throw new Error('Exponent must be a non-negative integer');
     }
@@ -7,14 +7,9 @@ function powerOfTwo(exponent) {
   }
   
   // Function to check if a number is a power of 2
-  function isPowerOfTwo(number) {
+  export function isPowerOfTwo(number) {
     if (number <= 0) {
       return false;
     }
     return (number & (number - 1)) === 0;
   }
-  
-  module.exports = {
-    powerOfTwo,
-    isPowerOfTwo,
-  };
